202520017f36aef509c4a74573ecacb274eb05e1
trisha-narwekar <trishaprtn@gmail.com>
2022-12-05 01:45:15 -0600

    almost done ;-;

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,8 +2,8 @@ export default function Navbar() {
     return <nav className="nav">
         <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/k12">K-12</a></li>
-            <li><a href="/university">Higher Education</a></li>
+            {/* <li><a href="/k12">K-12</a></li>
+            <li><a href="/university">Higher Education</a></li> */}
             <li><a href="/schedule">Schedule</a></li>
         </ul>
     </nav>
