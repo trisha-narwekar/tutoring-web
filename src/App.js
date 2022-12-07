c9fd6596c6e6762c8f492cdb9dd47e2a92095c8f
trisha-narwekar <trishaprtn@gmail.com>
2022-12-06 22:59:47 -0600

    po

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Schedule from "./pages/Schedule";
 import Navbar from "./pages/Navbar"
@@ -12,7 +12,6 @@ function App() {
     <Routes>
           <Route path='/' exact element={<Home/>} />
           <Route path='/schedule' exact element={<Schedule/>} />
-          /
       </Routes>
       </Router>
       </div>
