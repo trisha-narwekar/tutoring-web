fb9986a0cf9db0ad5bc400b009b55f5e251cce2a
trisha-narwekar <trishaprtn@gmail.com>
2022-12-11 20:27:11 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from "./pages/Home";
@@ -8,15 +8,14 @@ import Navbar from "./pages/Navbar"
 
 function App() {
   return (
-    <div>
-    <Navbar/>
-    <Router>
-    <Routes>
-          <Route path='/' exact element={<Home/>} />
-          <Route path='/schedule' exact element={<Schedule/>} />
-      </Routes>
+    <>
+      <Router>
+        <div>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/schedule' exact element={<Schedule />} />
+        </div>
       </Router>
-      </div>
+    </>
   );
 }
 
