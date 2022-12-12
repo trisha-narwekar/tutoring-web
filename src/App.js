0984f677e4b73ebcf1f40e206851a9dfbd10b582
trisha-narwekar <trishaprtn@gmail.com>
2022-12-11 20:22:31 -0600

    potato

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React, {useState, useEffect} from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
 import Home from "./pages/Home";
 import Schedule from "./pages/Schedule";
 import Navbar from "./pages/Navbar"
@@ -6,14 +8,15 @@ import Navbar from "./pages/Navbar"
 
 function App() {
   return (
-    <>
-      <Router>
-        <div>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/schedule' exact element={<Schedule />} />
-        </div>
+    <div>
+    <Navbar/>
+    <Router>
+    <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/schedule' exact element={<Schedule/>} />
+      </Routes>
       </Router>
-    </>
+      </div>
   );
 }
 
